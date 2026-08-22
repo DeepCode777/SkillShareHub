a8240f8dcc54e6abd969fff6114251912e2f1967
Deep Poptani <deeppoptani777@gmail.com>
2026-08-22 21:03:47 +0530

    Add AuthenticationFilter to manage access to protected resources

---
+++ src/com/skillsharehub/filter/AuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.skillsharehub.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/pages/*")
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String requestURI = httpRequest.getRequestURI();
+        String contextPath = httpRequest.getContextPath();
+
+        String requestedResource = requestURI.substring(contextPath.length());
+
+        if (isPublicResource(requestedResource)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        HttpSession session = httpRequest.getSession(false);
+
+        if (session != null && session.getAttribute("loggedInUser") != null) {
+
+            chain.doFilter(request, response);
+
+        } else {
+
+            httpResponse.sendRedirect( contextPath + "/pages/login.jsp");
+        }
+    }
+
+    private boolean isPublicResource(String resource) {
+
+        return resource.equals("/pages/login.jsp")
+                || resource.equals("/pages/register.jsp")
+                || resource.equals("/pages/login")
+                || resource.equals("/pages/register")
+                || resource.equals("/pages/logout");
+    }
+}
